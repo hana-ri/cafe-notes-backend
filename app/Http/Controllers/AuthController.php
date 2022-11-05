0299d13af0683f4a9a7203a232e1b31806962a8f
hana-ri <hm.rizal1933@gmail.com>
2022-11-05 19:30:27 +0700

    adding role to authControlle

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -54,6 +54,7 @@ class AuthController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'role' => $request->role,
             'password' => Hash::make($request->password),
         ]);
 
