ce0a45b8160200bdc2e00c1b477385406b3ef411
hana-ri <hm.rizal1933@gmail.com>
2022-11-06 16:40:13 +0700

    adding authentification to cateogry controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -10,6 +10,12 @@ use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+    
     /**
      * Display a listing of the resource.
      *
