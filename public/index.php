d8ea417e4f60a1783d0bad9072e63b5f8b130aa8
hana-ri <hm.rizal1933@gmail.com>
2022-11-17 14:55:54 +0700

    delete header cors to public/index, add api to array paths

--- public/index.php
+++ public/index.php
@@ -44,11 +44,6 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS, post, get');
-header("Access-Control-Max-Age", "3600");
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-header("Access-Control-Allow-Credentials", "true");
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
